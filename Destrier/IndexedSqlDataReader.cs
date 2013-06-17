859d5d94afebb31316b76cf704996d9cac8cce96
Will Charczuk <will@clotheshor.se>
2013-06-17 12:10:49 -0400

    more data types.

--- Destrier/IndexedSqlDataReader.cs
+++ Destrier/IndexedSqlDataReader.cs
@@ -275,7 +275,6 @@ namespace Destrier
                         default:
                             return _dr.GetString(i);
                     }
-                    
                 case TypeCode.DateTime:
                     return _dr.GetDateTime(i);
                 case TypeCode.Decimal:
