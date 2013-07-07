cb0a8f1702c56bfe0af1f53b9fae7519d2695e05
Will Charczuk <will@clotheshor.se>
2013-07-07 01:55:28 -0400

    moving these tests to indexed data reader file.

--- Destrier.Test/IndexedSqlDataReaderTests.cs
+++ Destrier.Test/IndexedSqlDataReaderTests.cs
@@ -239,5 +239,102 @@ namespace Destrier.Test
             Assert.NotNull(testObjects);
             Assert.NotEmpty(testObjects);
         }
+
+
+        [Fact]
+        public void Execute_StoredProcedureReader_Test()
+        {
+            var hasRows = false;
+            Execute.StoredProcedureReader("GetTestObjects_prc", (dr) =>
+            {
+                hasRows = dr.HasRows;
+            });
+
+            Assert.True(hasRows);
+        }
+
+        [Fact]
+        public void Execute_StoredProcedureReader_WithParameter_Test()
+        {
+            var objects = new List<TestObject>();
+            Execute.StoredProcedureReader("GetTestObjects_prc", (dr) =>
+            {
+                objects = dr.ReadList<TestObject>();
+            }, parameters: new { limit = 100 });
+
+            Assert.NotEmpty(objects);
+            Assert.True(objects.Count == 100);
+        }
+
+        [Fact]
+        public void Execute_NonQuery_Test()
+        {
+            Execute.NonQuery("update TestObjects set name = 'name_zero' where id = 1");
+
+            String newName = String.Empty;
+            Execute.StatementReader("select name from testobjects where id = 1", (dr) =>
+            {
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        newName = dr.Get<String>(0);
+                    }
+                }
+            });
+
+            Assert.Equal("name_zero", newName);
+        }
+
+        [Fact]
+        public void Execute_NonQuery_WithParam_Test()
+        {
+            Execute.NonQuery("update TestObjects set name = @name where id = @id", new { name = "name_zero", id = 1 });
+
+            String newName = String.Empty;
+            Execute.StatementReader("select name from testobjects where id = 1", (dr) =>
+            {
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        newName = dr.Get<String>(0);
+                    }
+                }
+            });
+
+            Assert.Equal("name_zero", newName);
+        }
+
+        [Fact]
+        public void Execute_StatementReader_WithParameter()
+        {
+            String newName = String.Empty;
+            Execute.StatementReader("select name from testobjects where id = @id", (dr) =>
+            {
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        newName = dr.Get<String>(0);
+                    }
+                }
+            }, parameters: new { id = 1 });
+
+            Assert.NotNull(newName);
+            Assert.NotEmpty(newName);
+        }
+
+        [Fact]
+        public void Execute_Utility_AddWhereClauseVariables()
+        {
+            StringBuilder builder = new StringBuilder();
+            var parameters = new { name = "test" };
+            Execute.Utility.AddWhereClauseVariables(parameters, builder);
+            var text = builder.ToString();
+
+            Assert.NotEmpty(text);
+            Assert.Equal("and [name] = @name\r\n", text);
+        }
     }
 }
